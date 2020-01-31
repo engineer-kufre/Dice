70af659c3e76437aa22973a629207f56b6ece761
engineer-kufre <engineer_kufre@yahoo.com>
2020-01-31 17:03:26 +0100

    updated js

--- dice.js
+++ dice.js
@@ -3,7 +3,8 @@ let dice2 = 0;
 
 const dice1_span = document.getElementById('dice1');
 const dice2_span = document.getElementById('dice2');
-const button = document.getElementById('button');
+const button = document.getElementById('roll');
+const reset = document.getElementById('reset');
 
 function roll() {
     return Math.floor(Math.random() * 7);
@@ -14,4 +15,9 @@ button.addEventListener('click', function() {
     dice2 = roll();
     dice1_span.innerHTML = dice1;
     dice2_span.innerHTML = dice2;
+})
+
+reset.addEventListener('click', function() {
+    dice1_span.innerHTML = 0;
+    dice2_span.innerHTML = 0;
 })
